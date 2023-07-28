d62fb3e1cffa22211a1df04566560ed82cd369ed
mkrishnapriya2003 <mkrishnapriya2003@gmail.com>
2023-07-28 21:11:44 +0530

    Uploaded project files

---
+++ Matsyastra/js/portfolio.js
@@ -0,0 +1,22 @@
+$(function(){
+	'use-strict';
+
+     // portfolio filter container
+    $('.filtr-container').imagesLoaded( function() {
+        var filterizr = $('.filtr-container').filterizr();
+    });
+
+    // portfolio filter
+    $('.simplefilter li').click(function() {
+        $('.simplefilter li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // portfolio image-popup
+    $(".image-popup").magnificPopup({
+        type: "image",
+        removalDelay: 300,
+        mainClass: "mfp-fade"
+    });
+
+});
